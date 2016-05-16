4933d3410f1131e690145cad8ff3f04648452f27
SeanGilhuly <seangilhuly@hotmail.com>
2016-05-16 15:01:49 -0600

    Set up storyboard
    Set up storyboard and files

---
+++ Alarm/AlarmDetailTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AlarmDetailTableViewController.swift
+//  Alarm
+//
+//  Created by Sean Gilhuly on 5/16/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
---
+++ Alarm/SwitchTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm
+//
+//  Created by Sean Gilhuly on 5/16/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var alarmSwitch: UISwitch!
+    
+    
+    // MARK: - IBActions
+    
+    
+    @IBAction func switchValueChanged(sender: AnyObject) {
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
